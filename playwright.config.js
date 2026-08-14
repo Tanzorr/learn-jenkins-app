c7f53d77d3da173a9ad858b3c33c37d0410a8201
tanzorr <alexx1984@ukr.net>
2026-08-14 10:04:49 +0300

    change/play write config

--- playwright.config.js
+++ playwright.config.js
@@ -64,7 +64,7 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: process.env.CI_ENVIRONMENT_URL ? undefined : {
-    command: 'node_modules/.bin/serve -s build -l 3000',
+    command: 'serve -s build -l 3000',
     url: 'http://localhost:3000',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
